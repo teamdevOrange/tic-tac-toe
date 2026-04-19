36f718cc59903350430d75d3b3ea6a8d7c1b59da
miranon <uguisu138@gmail.com>
2026-04-20 02:15:39 +0900

    Square.js・Board.jsのスケルトンを作りダミーデータで盤面を描画する

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,1 +1,10 @@
 // 1マスの描画・クリックイベント
+
+export function oneSquare(index, value) {
+  //div属性を追加する
+  const cell = document.createElement('div');
+  cell.className = 'square';
+  //もらった要素(×とかそういうの)を入れる
+  cell.textContent = value;
+  return cell;
+}
